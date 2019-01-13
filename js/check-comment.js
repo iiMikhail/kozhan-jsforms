0cbf09707f24788d0ceb08e064e4a00f8f0f6c2a
Michail <mikhail.kozhan@gmail.com>
2019-01-13 23:52:50 +0300

    complere

---
+++ js/check-comment.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+$('#error').hide();
+var validComment = (function(){
+	var button = $('#buttonComment');
+	var init = function(){
+		_setUpListeners();
+	}
+	var _setUpListeners = function() {
+		$('#buttonComment').on('click', function(event) {
+			validateForm(event);
+		});
+		$('.textarea').on('click', function() {
+			$('#error').hide();
+		});
+	}
+	var validateForm = function(event) {
+		event.preventDefault();
+		var form = $('form');
+		if( $('.textarea').val().trim() == '' ) {
+			$('#error').show();
+		} else {
+			$('#error').hide();
+		}	
+	}
+	return {
+		init
+	}
+}());
+validComment.init();
+});
---
+++ js/check-login.js
@@ -0,0 +1,67 @@
+$(document).ready(function() {
+	$('#nomail').hide();
+	$('#nopas').hide();
+	$('#fmail').hide();
+	$('#noacc').hide();
+
+	checkLogin = (function(){
+		var UserMail = 'mail@mail.ru',
+			UserPassword = '123';
+		var init = function() {
+			_setUpListeners();
+		}
+		var _setUpListeners = function() {
+			$('.button').on('click', function(event) {
+				validateForm(event);
+			});
+			$('#mail').on('click', function(event) {
+				$('#nomail').hide();
+				$('#fmail').hide();
+				$('#noacc').hide();
+			});
+			$('#password').on('click', function(event) {
+				$('#nopas').hide();
+				$('#noacc').hide();
+			});
+		}
+		var validateForm = function(event) {
+			event.preventDefault();
+			//  validate e-mail
+			if( $('#mail').val().trim() == '' ) {
+				$('#nomail').show();
+			} else {
+				$('#nomail').hide();
+			}
+			var patern = /^[a-z0-9_-]+@[a-z0-9-]+\.([a-z]{1,6}\.)?[a-z]{2,6}$/i;
+			// var patern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+			if ( patern.test( $('#mail').val() ) ){
+				$('#fmail').hide();
+			} else {
+				$('#fmail').show();
+			}
+
+			// validate password
+			if( $('#password').val().trim() == '' ) {
+				$('#nopas').show();
+			} else {
+				$('#nopas').hide();
+			}
+			// end validate password
+
+			// validate accaunt
+			if ( ( $('#mail').val() !== '' ) || ( $('#password').val() !== '' ) ) {
+				if( ( $('#mail').val() === UserMail ) && ( $('#password').val() === UserPassword ) ) {
+				$('#noacc').hide();
+				}	else {
+				$('#noacc').show();
+				}
+			}
+			// end validate account
+
+		}
+		return {
+			init
+		}
+	}());
+checkLogin.init();
+});
